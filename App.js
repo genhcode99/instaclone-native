9fb654cc6428c72503eedebc03a060fca49510c4
JeUpark <genhcode99@gmail.com>
2021-07-17 12:26:13 +0900

    #13.6 AppLoading

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import { StatusBar } from "expo-status-bar"
-import React from "react"
+import React, { useState } from "react"
+import AppLoading from "expo-app-loading"
+import * as Font from "expo-font"
+import { Ionicons } from "@expo/vector-icons"
 import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
+  // *[ States ]*
+  const [loading, setLoading] = useState(true)
+
+  // *[ Loading ]*
+  const startAsync = () => {
+    const fontsToLoad = [Ionicons.font]
+    const fontsPromises = fontsToLoad.map((font) => Font.loadAsync(font))
+    console.log(fontsPromises)
+    return Promise.all(fontsPromises)
+  }
+  const onFinish = () => setLoading(false)
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={startAsync}
+        onFinish={onFinish}
+        onError={console.warn}
+      />
+    )
+  }
+
+  // *[ Screen ]*
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app</Text>
