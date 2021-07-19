cf48ac5189640fbe631645fe0f0be07ba1038792
JeUpark <genhcode99@gmail.com>
2021-07-19 13:21:38 +0900

    #15.0 Tab Navigator

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,14 +1,61 @@
 import React from "react"
-import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Ionicons } from "@expo/vector-icons"
 import Feed from "../screens/Feed"
+import Search from "../screens/Search"
+import Notifications from "../screens/Notifications"
+import Profile from "../screens/Profile"
 
 const Taps = createBottomTabNavigator()
 
 const LoggedInNav = () => {
   return (
-    <Taps.Navigator>
-      <Taps.Screen name="Feed" component={Feed} />
+    <Taps.Navigator
+      tabBarOptions={{
+        activeTintColor: "white",
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "rgba(255,225,225,0.3)",
+        },
+      }}
+    >
+      <Taps.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="home" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
+      <Taps.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="search" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
+      <Taps.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="heart" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
+      <Taps.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="person" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
     </Taps.Navigator>
   )
 }
