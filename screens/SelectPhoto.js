8721520f85de13adda3cdcdda8d4f1b636be68df
JeUpark <genhcode99@gmail.com>
2021-07-22 14:23:10 +0900

    #17.3 Select Photo part Two

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import * as MediaLibrary from "expo-media-library"
 import styled from "styled-components/native"
 
 const Container = styled.View`
@@ -13,10 +14,37 @@ const Bottom = styled.View`
 `
 
 const SeletPhoto = () => {
+  const [ok, setOk] = useState(false)
+  const [photos, setPhotos] = useState([])
+
+  const getPermission = async () => {
+    const { accessPrivileges, canAskAgain } =
+      await MediaLibrary.getPermissionsAsync()
+    if (accessPrivileges === "none" && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync()
+      if (accessPrivileges !== "none") {
+        setOk(true)
+      }
+    } else if (accessPrivileges !== "none") {
+      setOk(true)
+    }
+  }
+
+  const getPhotos = async () => {
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync()
+      setPhotos(photos)
+    }
+  }
+
+  useEffect(() => {
+    getPermission()
+    getPhotos()
+  }, [])
   return (
     <Container>
       <Top />
-      <Bottom />
+      <Bottom></Bottom>
     </Container>
   )
 }
