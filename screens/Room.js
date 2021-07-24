7d5891fd925db3554b133d82f0672ad51ea95dea
JeUpark <genhcode99@gmail.com>
2021-07-24 12:49:09 +0900

    #18.5 Room Screen part Three

--- screens/Room.js
+++ screens/Room.js
@@ -5,22 +5,37 @@ import { gql, useQuery } from "@apollo/client"
 import ScreenLayout from "../components/ScreenLayout"
 
 // =====< Style >=====
-const MessageContainer = styled.View``
+const MessageContainer = styled.View`
+  padding: 10px 0px;
+  flex-direction: ${(props) => (props.outGoing ? "row" : "row-reverse")};
+  align-items: flex-end;
+`
 const Author = styled.View``
-const Avatar = styled.Image``
+const Avatar = styled.Image`
+  width: 20px;
+  height: 20px;
+  border-radius: 9999px;
+`
 const Username = styled.Text`
   color: white;
 `
 const Message = styled.Text`
   color: white;
+  font-size: 16px;
+  margin: 0px 10px;
+  overflow: hidden;
+  padding: 5px 10px;
+  border-radius: 10px;
+  background-color: rgba(225, 225, 225, 0.3);
 `
 const TextInput = styled.TextInput`
   width: 95%;
-  margin: 0px 10px;
+  color: white;
+  margin-top: 25px;
   padding: 10px 20px;
   margin-bottom: 50px;
   border-radius: 9999px;
-  background-color: white;
+  border: 1px solid rgba(255, 255, 255, 0.5);
 `
 
 // =====< GraphQl >=====
@@ -39,6 +54,14 @@ const ROOM_QUERY = gql`
     }
   }
 `
+const SEND_MESSAGE_MUTATION = gql`
+  mutation sendMessage($payload: String!, $roomId: Int, $userId: Int) {
+    sendMessage(payload: $payload, roomId: $roomId, userId: $userId) {
+      ok
+      id
+    }
+  }
+`
 
 // =====< Component >=====
 const Room = ({ route, navigation }) => {
@@ -48,6 +71,8 @@ const Room = ({ route, navigation }) => {
   const { data, loading } = useQuery(ROOM_QUERY, {
     variables: { id: route?.params?.id },
   })
+
+  //
 
   // Use Effect
   useEffect(() => {
@@ -60,10 +85,11 @@ const Room = ({ route, navigation }) => {
 
   // Flat List
   const renderItem = ({ item: message }) => (
-    <MessageContainer>
+    <MessageContainer
+      outGoing={message.user.username === route?.params?.talkingTo?.username}
+    >
       <Author>
         <Avatar source={{ uri: message.user.avatar }} />
-        <Username>{message.user.username}</Username>
       </Author>
       <Message>{message.payload}</Message>
     </MessageContainer>
@@ -72,8 +98,8 @@ const Room = ({ route, navigation }) => {
   // Main Screen
   return (
     <KeyboardAvoidingView
-      behavior="height"
-      keyboardVerticalOffset={100}
+      behavior="padding"
+      keyboardVerticalOffset={70}
       style={{ flex: 1, backgroundColor: "black" }}
     >
       <ScreenLayout loading={loading}>
@@ -88,7 +114,7 @@ const Room = ({ route, navigation }) => {
           placeholder="Write a message..."
           // onSubmitEditing={}
           returnKeyType="send"
-          placeholderTextColor="rgba(0,0,0,0.5)"
+          placeholderTextColor="rgba(225,225,225,0.5)"
         />
       </ScreenLayout>
     </KeyboardAvoidingView>
